3df0e3af966e7e09bdc543b61fe5e4f1ed6df0ff
Alexandre Pinto <alexj@arpaeden.local>
2020-11-16 19:44:06 +0000

    adds new possible interaction w/ moulinette 🥰

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('Amo-te'):
+        await message.channel.send('só amo o Miguel Gueifão...!')
+
+    if "slack" in message.content:
+        await message.channel.send('do you miss the 🚨 slack police 🚨')
+
+    if "vs code" in message.content:
+        await message.channel.send('Hey! Here are the 🚨 top 10 💥 reasons to use VSCode: \n (null)')
 
 client.run("Nzc3NjQwNDQ0NDk4MjgwNDU4.X7GYGQ.N7yiq0mVef0Y8Va4NKIecFz4hqk")
