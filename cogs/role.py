d2df9f1fabcae812eec43c4f8dc7e5a768a41eb0
candec <omnius.candec@gmail.com>
2020-11-17 14:34:20 +0000

    roles

---
+++ cogs/role.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+client = discord.Client()
+
+class Roles(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+@commands.Cog.Listener()
+async def on_raw_reaction_add(payload):
+    massage_id = patload.message_id
+    if message_id == 778231967761563669:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)  
+
+    if role is not None:
+        member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+        if memeber is not None:
+            await member.add_roles(role)
+
+
+@commands.Cog.Listener()
+async def on_raw_reaction_add(payload):
+    massage_id = patload.message_id
+    if message_id == 778231967761563669:
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+        role = discord.utils.get(guild.roles, name=payload.emoji.name)  
+
+    if role is not None:
+        member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+        if memeber is not None:
+            await member.remove_roles(role)
+
+
+
+@commands.Cog.Listener()
+async def on_raw_reaction_remove(payload):
+    pass
