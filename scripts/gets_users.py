c89b3c6c4ac516e86ae3802147bd552ebaa679d4
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-03-04 11:26:01 +0000

    updates current state of API calls, but this is not a reasonable checkpoint! ⚠️ Work is mostly incomplete

--- scripts/gets_users.py
+++ scripts/gets_users.py
@@ -1,5 +1,5 @@
 # What this script does:
-# Fetches all users from the campus, retaining unique ID's. Updates the database into a neat JSON
+# Fetches all users from the campus, retaining unique ID's. Makes a json database matching name to ID.
 # currently hardcoded for 42 Lisboa's data; should be easily changeable.
 
 import requests
@@ -16,6 +16,25 @@ users = {}
 while (i == 1 or response != []):
 	response = gt.token_request(campus_users_endpoint, i).json()
 	for info in response:
-		users[info['login']] = info['id']
+		users[info['login']] = {}
+		users[info['login']]['id'] = info['id']
+		user_endpoint = f'v2/users/{info["id"]}'
+		tries = 0
+		success = 0
+		while (tries < 3 and not success):
+			try:
+				response = gt.token_request(user_endpoint).json()
+				if 21 == response['cursus_users'][1]['cursus_id']:
+					users[info['login']]['is_student'] = True
+				print(f"user {[info['login']]} added!")
+				success = 1
+				break
+			except IndexError:
+				break
+			except Exception:
+				time.sleep(2)
+				tries += 1
+				print('wasting time!')
+
 	i += 1
 dj.dump_to_json("users_ids", users)
