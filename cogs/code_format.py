f91b9fd44b55b90bc3d98a57a8854cc32e174f2e
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-05-22 05:23:04 +0100

    hotfixes code_format 🔥; adds pinned_messages 📌

--- cogs/code_format.py
+++ cogs/code_format.py
@@ -20,14 +20,14 @@ class CodeFormat(commands.Cog):
 	async def code(self, ctx):
 		await ctx.message.delete()
 		self.sent_text_message = await ctx.message.channel.send("For better readability and syntax formatting, please format your code accordingly! 🤖\n`` ``` [language]\n[code]\n``` ``\n where ``[language]`` can be ``C``, ``Python``, etc. and ``[code]`` is... well, your code. ⌨️\nClick on ✅ to remove the message when you're done.")
-		self.sent_image_message = await ctx.message.channel.send("https://bzzzzzz.buzz/static/print.png")
+		self.sent_image_message = await ctx.message.channel.send("https://bzzzzzzz.buzz/static/print.png")
 		await self.sent_image_message.add_reaction('✅')
 
 	@commands.Cog.listener()
 	async def on_raw_reaction_add(self, ctx):
 		if ctx.member == self.client.user:
 			return
-		if ctx.message_id == self.sent_image_message.id and ctx.emoji.name == '✅':
+		if ctx.emoji.name == '✅' and ctx.message_id == self.sent_image_message.id:
 			await self.sent_text_message.delete()
 			await self.sent_image_message.delete()
 
